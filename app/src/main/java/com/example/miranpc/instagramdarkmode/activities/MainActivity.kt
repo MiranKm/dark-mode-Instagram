aef365ed440e93a66dd11b76799a46c2877b0709
MiranPC <miran9191@gmail.com>
2019-08-08 23:58:22 +0300

    adds the bottom navigation dependencies withe setting up the navigation controller

--- app/src/main/java/com/example/miranpc/instagramdarkmode/activities/MainActivity.kt
+++ app/src/main/java/com/example/miranpc/instagramdarkmode/activities/MainActivity.kt
@@ -2,16 +2,28 @@ package com.example.miranpc.instagramdarkmode.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.miranpc.instagramdarkmode.R
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        navController = Navigation.findNavController(this, R.id.container)
+        bottom_nav.setupWithNavController(navController)
 
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 
 }
