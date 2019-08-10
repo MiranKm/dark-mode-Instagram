9e9d133d6d79e3df62e440388e82ea01c863c2ed
MiranPC <miran9191@gmail.com>
2019-08-10 19:23:29 +0300

    changes the location of the main feed toolbar into the feeds fragment

--- app/src/main/java/com/example/miranpc/instagramdarkmode/activities/MainActivity.kt
+++ app/src/main/java/com/example/miranpc/instagramdarkmode/activities/MainActivity.kt
@@ -19,13 +19,36 @@ class MainActivity : AppCompatActivity() {
 
         navController = Navigation.findNavController(this, R.id.container)
         bottom_nav.setupWithNavController(navController)
+        /* val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+         setSupportActionBar(toolbar)
+         val igHome= toolbar.findViewById<View>(R.id.ig_icon)
+         val dmHome= toolbar.findViewById<View>(R.id.dm_icon)
+
+         igHome.setOnClickListener{
+             Toast.makeText(this, "IG", Toast.LENGTH_SHORT).show()
+         }
+         dmHome.setOnClickListener{
+             Toast.makeText(this, "Direct Message", Toast.LENGTH_SHORT).show()
+         }*/
 
 //        bottom_nav.itemIconTintList = null;
 
     }
+/*
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(this).inflate(R.menu.main_menu, menu)
+        return true
+    }*/
 
     override fun onNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, null)
     }
-
+/*
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.navigation_message-> Toast.makeText(this, "DM", Toast.LENGTH_SHORT).show()
+            R.id.navigation_home -> Toast.makeText(this, "IG", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }*/
 }
